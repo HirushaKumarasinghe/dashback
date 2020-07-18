f25d290bc04370263df759ea45563a3738632db8
IT17003988 <it17003988@my.sliit.lk>
2020-07-19 00:32:28 +0530

    geo

--- app/routes/trend.js
+++ app/routes/trend.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 const trend = require('../controllers/trend.controller');
 
-router.post('/drop',trend.drop);
+router.get('/test',trend.test);
 router.post('/recruit',trend.employeeFilter);
 
 module.exports = router;
